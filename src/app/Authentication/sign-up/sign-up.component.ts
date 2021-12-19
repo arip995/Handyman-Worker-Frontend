cf42a1f618e808e504c0d94826ca4b878931ace3
arip995 <arindampanda2002@gmail.com>
2021-12-19 08:00:18 +0530

    created signup component

---
+++ src/app/Authentication/sign-up/sign-up.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'sign-up',
+  templateUrl: './sign-up.component.html'
+})
+export class SignUpComponent implements OnInit {
+  signUpForm: FormGroup;
+
+  //Constructor
+  constructor(
+    private _formBuilder: FormBuilder
+  ){
+    this.signUpForm = _formBuilder.group({
+      name: new FormControl("",[Validators.required]),
+      mobileNumber : new FormControl("",[Validators.required,Validators.maxLength(10),Validators.minLength(10)]),
+      username : new FormControl("",[Validators.required]),
+      password : new FormControl("",[Validators.required]),
+      confirmPassword : new FormControl("",[Validators.required])
+    });
+  }
+
+
+  //OnInit Method
+ngOnInit(){
+  // this.signUpForm.reset({
+
+  // })
+}
+
+checkError(errorName:any){
+  return()=>{
+    this.signUpForm.controls['name'].hasError(errorName);
+    this.signUpForm.controls['mobileNumber'].hasError(errorName);
+    this.signUpForm.controls['username'].hasError(errorName);
+    this.signUpForm.controls['password'].hasError(errorName);
+    this.signUpForm.controls['confirmPassword'].hasError(errorName);
+  } 
+}
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { SignUpComponent } from './Authentication/sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path       : '',
+    pathMatch  : 'full',
+    redirectTo : '/sign-up',
+    data       : {title: 'Sign-up'}
+},{
+  path       : 'sign-up',
+  pathMatch  : 'full',
+  component  : SignUpComponent,
+},
+// Admin routes
+// {
+//   path       : '',
+//   children   : [
+//       {path: 'SignUpComponent', loadChildren: () => import('./Authentication/authentication.module').then(m => m.AuthenticationModule)},
+//   ]
+// }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
